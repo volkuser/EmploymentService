a1714bbcacf49bd751ab481858ada42c1bd3a95d
daniel demidov <demidov008h@gmail.com>
2022-10-22 22:44:19 +0300

    new structure, some fixes, mapping

---
+++ Apps/Controls/Common/Mappings/IMapWith.cs
@@ -0,0 +1,9 @@
+using AutoMapper;
+
+namespace Controls.Common.Mappings;
+
+public interface IMapWith<T>
+{
+    void Mapping(Profile profile)
+        => profile.CreateMap(typeof(T), GetType());
+}
---
+++ Apps/WebAPI/Controllers/ProfessionController.cs
@@ -0,0 +1,69 @@
+using DbInteraction;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/profession")]
+public class ProfessionController : Controller
+{
+    private readonly ServiceDbContext _dbContext;
+
+    public ProfessionController(ServiceDbContext dbContext) => _dbContext = dbContext;
+    
+    [HttpGet]
+    public Task<ActionResult> GetProfessions()
+    {
+        return Task.FromResult<ActionResult>(Ok(_dbContext.Professions!.ToList()));
+    }
+    
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult> GetProfessions([FromRoute] int id)
+    {
+        var profession = await _dbContext.Professions!.FindAsync(id);
+
+        if (profession != null) return Ok(profession);
+        
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> InsertProfession(Profession profession)
+    {
+        await _dbContext.Professions!.AddAsync(profession);
+        await _dbContext.SaveChangesAsync();
+
+        return Ok(profession);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateProfession(Profession? profession)
+    {
+        if (profession?.Id != null)
+        {
+            var updatingProfession = await _dbContext.Professions!.FindAsync(profession.Id);
+            updatingProfession!.Name = profession.Name;
+            await _dbContext.SaveChangesAsync();
+            
+            return Ok(profession);
+        }
+
+        return NotFound();
+    }
+    
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<ActionResult> DeleteProfession([FromRoute] int id)
+    {
+        var profession = await _dbContext.Professions!.FindAsync(id);
+
+        if (profession == null) return NotFound();
+
+        _dbContext.Remove(profession);
+        await _dbContext.SaveChangesAsync();
+        
+        return Ok(profession);
+    }
+}
